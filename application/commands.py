aaee2b2501a407df2e0ace28326e5032b94868ae
Angel <angel.angel802@gmail.com>
2019-03-18 13:28:15 +0100

    Adding the summoner command

--- application/commands.py
+++ application/commands.py
@@ -1,10 +1,22 @@
 # coding=utf-8
+import requests
+
 from application import bot
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(commands=['summoner'])
+def summoner(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+    r = requests.get('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/anthares101?api_key=' + RIOT_KEY)
+    content = r.json()
+
+    bot.reply_to(message, content["name"])
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
