0ba98a12e848a3d306a8eb1ebafe6d82f0a93b5e
Angel <angel.angel802@gmail.com>
2019-03-18 17:26:38 +0100

    summoner command still in progress

--- application/commands.py
+++ application/commands.py
@@ -19,18 +19,22 @@ def summoner(message):
         bot.reply_to(message, "Especifique un nombre de invocador")
     else:
         cid = chat_id = message.chat.id
-        region = Chat.get_config(cid, "region").value
-        url = 'https://'+region+'.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+name
-        params = {
-            'api_key': RIOT_KEY
-        }
-        r = requests.get(url, params)
+        region = Chat.get_config(cid, "region")
 
-        if(r.status_code in range(200,299)):#Request accepted
-            content = r.json()
-            bot.reply_to(message, content["name"])
-        else:#Request error
-            bot.reply_to(message, "Invocador desconocido")
+        if region is None:
+            bot.send_message(message.chat.id, "Utiliza /region para seleccionar una region primero")
+        else:
+            url = 'https://'+region.value+'.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+name
+            params = {
+                'api_key': RIOT_KEY
+            }
+            r = requests.get(url, params)
+
+            if(r.status_code in range(200,299)):#Request accepted
+                content = r.json()
+                bot.reply_to(message, content["name"])
+            else:#Request error
+                bot.reply_to(message, "Invocador desconocido")
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
